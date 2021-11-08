b3289e20d12d129b6736341b5bdce5f6f1eef6f5
syedmudassir <sidimran4455@gmail.com>
2021-11-08 12:05:38 +0530

    remove files

--- src/components/Expenses/ExpensesList.jsx
+++ src/components/Expenses/ExpensesList.jsx
@@ -18,18 +18,7 @@ const ExpensesList = (props) => {
     </ul>
   );
 
-//   return (
-//     <ul className='expenses-list'>
-//       {props.items.map((expense) => (
-//         <ExpenseItem
-//           key={expense.id}
-//           title={expense.title}
-//           amount={expense.amount}
-//           date={expense.date}
-//         />
-//       ))}
-//     </ul>
-//   );
+
 };
 
 export default ExpensesList;
